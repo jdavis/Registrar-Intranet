cf830e9218cd7d114e5681bb385f927f628a7c8c
Josh Davis <joshuadavis42@gmail.com>
2011-09-20 12:32:54 -0500

    Fixed the pre-deployment script, again.

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -1,13 +1,21 @@
 # Make the tmp and cache directories
-mkdir app/tmp
-mkdir app/tmp/cache
-mkdir cake/libs/cache
+echo Creating app/tmp && mkdir app/tmp > /dev/null 2>&1
+echo Creating app/tmp/cache && mkdir app/tmp/cache > /dev/null 2>&1
+echo Creating app/tmp/cache/persistent/ && mkdir app/tmp/cache/persistent/ > /dev/null 2>&1
+echo Creating app/tmp/cache/models/ && mkdir app/tmp/cache/models/ > /dev/null 2>&1
+echo Creating cake/libs/cache && mkdir cake/libs/cache > /dev/null 2>&1
+
 
 # Ensure that the folders are writable otherwise PHP will cry.
-fs sa app/tmp/cache vwh write
-fs sa cake/libs/cache vwh write
+echo
+echo Making app/tmp/ writable && fs sa app/tmp vwh write
+echo Making app/tmp/cache writable && fs sa app/tmp/cache vwh write
+echo Making app/tmp/cache/persistent writable && fs sa app/tmp/cache/persistent vwh write
+echo Making cake/libs/cache writable && fs sa cake/libs/cache vwh write
 
 # Copy over the config files from the development branch
-cp -a ../development/app/config app/
+echo
+echo Copying over development config... && cp -a ../development/app/config app/
 
+echo
 echo "Ready for take off."
