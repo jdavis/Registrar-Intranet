9fe9deb4552e282d396f036ba20f720f7b4a5e0d
Josh Davis <joshuadavis42@gmail.com>
2011-09-20 12:21:16 -0500

    Fixing the deployment script...

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -1,5 +1,13 @@
+# Make the tmp and cache directories
+mkdir app/tmp
+mkdir app/tmp/cache
+mkdir cake/libs/cache
+
 # Ensure that the folders are writable otherwise PHP will cry.
 fs sa app/tmp/cache vwh write
 fs sa cake/libs/cache vwh write
+
+# Copy over the config files from the development branch
+cp -a ../development/app/config app/
 
 echo "Ready for take off."
